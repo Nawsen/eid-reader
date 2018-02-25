641980c6782ef70d3c3ac7291aa7c8b4c887afcd
Wannes Van Dorpe <wannes.vandorpe@realdolmen.com>
2018-02-25 13:00:04 +0100

    :sparkles: Added dateOfBirth to output

--- src/main/java/be/wannesvandorpe/eid/BeIDCardReader.java
+++ src/main/java/be/wannesvandorpe/eid/BeIDCardReader.java
@@ -11,42 +11,42 @@ import java.io.IOException;
 
 public class BeIDCardReader {
 
-  public void readIdentityAndAddress() throws InterruptedException {
-    final BeIDCards beIDCards = new BeIDCards();
+    public void readIdentityAndAddress() throws InterruptedException {
+        final BeIDCards beIDCards = new BeIDCards();
 
-    try {
-      final BeIDCard card = beIDCards.getOneBeIDCard();
-      CitizenJson citizenJson = new CitizenJson();
-      try {
-        //get the identity from card
-        final byte[] idData = card.readFile(FileType.Identity);
-        final Identity id = TlvParser.parse(idData, Identity.class);
-        citizenJson.addIdentity(id);
+        try {
+            final BeIDCard card = beIDCards.getOneBeIDCard();
+            CitizenJson citizenJson = new CitizenJson();
+            try {
+                //get the identity from card
+                final byte[] idData = card.readFile(FileType.Identity);
+                final Identity id = TlvParser.parse(idData, Identity.class);
+                citizenJson.addIdentity(id);
 
-        //get the address from card
-        final byte[] addressData = card.readFile(FileType.Address);
-        final Address address = TlvParser.parse(addressData, Address.class);
-        citizenJson.addAddress(address);
+                //get the address from card
+                final byte[] addressData = card.readFile(FileType.Address);
+                final Address address = TlvParser.parse(addressData, Address.class);
+                citizenJson.addAddress(address);
 
-        //convert to json and send it out
-        String json = new ObjectMapper().writeValueAsString(citizenJson);
-        System.out.println(json);
+                //convert to json and send it out
+                String json = new ObjectMapper().writeValueAsString(citizenJson);
+                System.out.println(json);
 
-      } catch (final CardException cex) {
-        cex.printStackTrace();
-      } catch (final IOException iox) {
-        iox.printStackTrace();
-      }
-    } catch (final CancelledException cex) {
-      System.out.println("Cancelled By User");
+            } catch (final CardException cex) {
+                cex.printStackTrace();
+            } catch (final IOException iox) {
+                iox.printStackTrace();
+            }
+        } catch (final CancelledException cex) {
+            System.out.println("Cancelled By User");
+        }
+
     }
 
-  }
-
-  public static void main(final String[] args) throws InterruptedException,
-    BeIDCardsException {
-    final BeIDCardReader reader = new BeIDCardReader();
-    reader.readIdentityAndAddress();
-  }
+    public static void main(final String[] args) throws InterruptedException,
+            BeIDCardsException {
+        final BeIDCardReader reader = new BeIDCardReader();
+        reader.readIdentityAndAddress();
+    }
 
 }
--- src/main/java/be/wannesvandorpe/eid/CitizenJson.java
+++ src/main/java/be/wannesvandorpe/eid/CitizenJson.java
@@ -5,53 +5,59 @@ import be.fedict.commons.eid.consumer.Identity;
 
 public class CitizenJson {
 
-  private String name;
-  private String firstName;
-  private String nationalNumber;
-  private String placeOfBirth;
+    private String name;
+    private String firstName;
+    private String nationalNumber;
+    private String placeOfBirth;
+    private String dateOfBirth;
 
-  private String streetAndNumber;
-  private String zip;
-  private String municipality;
+    private String streetAndNumber;
+    private String zip;
+    private String municipality;
 
-  public void addIdentity(Identity id) {
-    this.name = id.getName();
-    this.firstName = id.getFirstName();
-    this.nationalNumber = id.getNationalNumber();
-    this.placeOfBirth = id.getPlaceOfBirth();
-  }
+    public void addIdentity(Identity id) {
+        this.name = id.getName();
+        this.firstName = id.getFirstName();
+        this.nationalNumber = id.getNationalNumber();
+        this.placeOfBirth = id.getPlaceOfBirth();
+        this.dateOfBirth = id.getDateOfBirth().toString();
+    }
 
-  public void addAddress(Address address) {
-    this.streetAndNumber = address.getStreetAndNumber();
-    this.zip = address.getZip();
-    this.municipality = address.getMunicipality();
-  }
+    public void addAddress(Address address) {
+        this.streetAndNumber = address.getStreetAndNumber();
+        this.zip = address.getZip();
+        this.municipality = address.getMunicipality();
+    }
 
-  public String getName() {
-    return name;
-  }
+    public String getName() {
+        return name;
+    }
 
-  public String getFirstName() {
-    return firstName;
-  }
+    public String getFirstName() {
+        return firstName;
+    }
 
-  public String getNationalNumber() {
-    return nationalNumber;
-  }
+    public String getNationalNumber() {
+        return nationalNumber;
+    }
 
-  public String getPlaceOfBirth() {
-    return placeOfBirth;
-  }
+    public String getPlaceOfBirth() {
+        return placeOfBirth;
+    }
 
-  public String getStreetAndNumber() {
-    return streetAndNumber;
-  }
+    public String getStreetAndNumber() {
+        return streetAndNumber;
+    }
 
-  public String getZip() {
-    return zip;
-  }
+    public String getZip() {
+        return zip;
+    }
 
-  public String getMunicipality() {
-    return municipality;
-  }
+    public String getMunicipality() {
+        return municipality;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
 }
